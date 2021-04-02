8e3bfc8e8742dbc6f6d40e30d856d523c0fedf93
rahat2020 <bloodyhell4040@gmail.com>
2021-04-03 01:58:30 +0600

    connecting to database

--- index.js
+++ index.js
@@ -17,26 +17,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ixqz.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productCollection = client.db("fruits").collection("Items");
   
-  app.post('/addProduct', (req, res) =>{
-    const product = req.body;
-    productCollection.insertMany(product)
-    .then(result => {
-      console.log(result.insertedCount);
-      res.send(result.insertedCount)
-    })
+app.post('/addProduct', (req, res) =>{
+  const product = req.body;
+  console.log(product)
+  console.log('add product responsed', product)
+  productCollection.insertOne(product)
+  .then(result => {
+    console.log(result.insertedCount > 0);
+    res.send(result.insertedCount > 0)
   })
-
-  app.get('/products', (req, res) =>{
-    productCollection.find({})
-    .toArray((err, documents) =>{
-      res.send(documents);
-    })
-  })
+})
+  
 
   console.log('database connected successfully')
  

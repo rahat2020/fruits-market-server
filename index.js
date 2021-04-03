0790b9f99494dd9d65cef130b7ca8e515043e0eb
rahat2020 <bloodyhell4040@gmail.com>
2021-04-04 01:27:40 +0600

    almost finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-
+// const ObjectID = require('mongodb').ObjectID;
 const app = express()
 require('dotenv').config()
 
@@ -21,6 +21,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productCollection = client.db("fruits").collection("Items");
+  const itemsCollection = client.db("fruits").collection("ItemsOrder");
   
 app.post('/addProduct', (req, res) =>{
   const product = req.body;
@@ -32,8 +33,45 @@ app.post('/addProduct', (req, res) =>{
     res.send(result.insertedCount > 0)
   })
 })
-  
 
+app.post('/itemOrdered', (req, res) =>{
+  const product = req.body;
+  console.log(product)
+  console.log('add product responsed', product)
+  itemsCollection.insertOne(product)
+  .then(result => {
+    console.log(result.insertedCount > 0);
+    res.send(result.insertedCount > 0)
+  })
+})
+
+app.get('/products', (req, res) =>{
+  productCollection.find({id:req.params.id})
+  .toArray((err, documents) =>{
+    res.send(documents);
+  })
+})
+
+app.get('/product/:id', (req, res) =>{
+  productCollection.find({_id:req.params.id})
+  .toArray((err, documents) =>{
+    res.send(documents);
+  })
+})
+
+app.get('/ordered', (req, res) =>{
+  itemsCollection.find({email: req.query.email})
+  .toArray((err, documents) =>{
+    res.send(documents)
+  })
+})
+
+// app.delete('deleteItems/:id', (req, res) => {
+//   const id = ObjectID(req.params.id);
+//   console.log('delete this item from here', id);
+//   eventCollection.findOneAndDelete({_id: id})
+//   .then(documents => res.send(!!documents.value))
+// })
   console.log('database connected successfully')
  
 });
